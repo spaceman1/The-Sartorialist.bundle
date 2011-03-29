844d6bfbb2b3598db735917c98f2bed5cbbcffd5
ryan <ryan@Mac-Pro.local>
2011-03-29 13:53:43 -0400

    Updated to v2

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,23 +1,21 @@
-from PMS import Plugin, Log, DB, Thread, XML, HTTP, JSON, RSS, Utils
-from PMS.MediaXML import MediaContainer, DirectoryItem, PhotoItem
 import re
-
 PLUGIN_PREFIX   = "/photos/TheSartorialist"
 RSS_FEED        = "http://feeds.feedburner.com/TheSartorialist"
 
 ####################################################################################################
 def Start():
-  Plugin.AddRequestHandler(PLUGIN_PREFIX, HandlePhotosRequest, "The Sartorialist", "icon-default.png", "art-default.jpg")
-  Plugin.AddViewGroup("Pictures", viewMode="Pictures", contentType="photos")
+	Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "The Sartorialist", "icon-default.png", "art-default.jpg")
+	Plugin.AddViewGroup("Pictures", viewMode="Pictures", mediaType="photos")
+	MediaContainer.art = R('art-default.jpg')
+	MediaContainer.viewGroup = 'Pictures'
 
 ####################################################################################################
-def HandlePhotosRequest(pathNouns, count):
-  dir = MediaContainer("art-default.jpg", "Pictures", "The Sartorialist")
-  
-  if count == 0:
-    for item in RSS.Parse(RSS_FEED).entries:
-      node = XML.ElementFromString(item.content[0].value, True)
-      img = node.xpath("//img")[0].get('src')
-      dir.AppendItem(PhotoItem(img, re.sub(r'[^.]+\.+', '', item.title), '', img))
+def MainMenu():
+	dir = MediaContainer(title1="The Sartorialist")
+	for item in RSS.FeedFromURL(RSS_FEED).entries:
+		title = item.title
+		node = HTML.ElementFromString(item.content[0].value)
+		img = node.xpath("//img")[0].get('src')
+		dir.Append(PhotoItem(img, re.sub(r'[^.]+\.+', '', item.title), '', img))
 
-  return dir.ToXML()
+	return dir
